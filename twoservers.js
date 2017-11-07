f501dcd86d5a00d7d8e3c52f2b7a207488fbfe87
TJ Cooper <tjcoop321@gmail.com>
2017-11-06 19:36:34 -0800

    test with two servers

---
+++ twoservers.js
@@ -0,0 +1,29 @@
+const http = require("http");
+
+// Define a port to listen for incoming requests
+const PORT = 7000;
+const PORT2 = 7500;
+
+// Create a generic function to handle requests and responses
+function handleRequest(request, response) {
+
+  // Send the below string to the client when the user visits the PORT URL
+  response.end("It Works!! Path Hit: " + request.url);
+}
+
+// Use the Node HTTP package to create our server.
+// Pass the handleRequest function to empower it with functionality.
+var server = http.createServer(handleRequest);
+
+// Start our server so that it can begin listening to client requests.
+server.listen(PORT, function() {
+
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+  console.log("You're looking very good");
+});
+
+server.listen(PORT2, function() {
+	console.log("Server listening on: http://localhost:" + PORT2);
+	console.log("You need to do better");
+})
